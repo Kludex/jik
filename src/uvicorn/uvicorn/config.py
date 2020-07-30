Kludex/jik

commit c184d5d2ae7676fcaef3623e15e57b5ebc52112c

Added asgi version and spec_version to scope (#597)
* Added asgi version and spec_version to scope
Minor import sort corrected in a test

* Added correct asgi version depending on interface used

* Lint

* Forgot websockets

* Added test for auto discovery of asgi interface

* Seperated config scope for asgi and protocol scope test

* Testing asgi_version

* Dont need anything in tested fake apps

* Flake8 !

src/uvicorn/uvicorn/config.py
@@ -201,6 +201,10 @@ class Config:
             self.forwarded_allow_ips = forwarded_allow_ips
 
     @property
+    def asgi_version(self) -> str:
+        return {"asgi2": "2.0", "asgi3": "3.0"}[self.interface]
+
+    @property
     def is_ssl(self) -> bool:
         return bool(self.ssl_keyfile or self.ssl_certfile)
 

src/uvicorn/tests/test_config.py
@@ -66,3 +66,19 @@ def test_ssl_config(certfile_and_keyfile):
     config.load()
 
     assert config.is_ssl is True
+
+
+def asgi2_app(scope):
+    async def asgi(receive, send):
+        pass
+
+    return asgi
+
+
+@pytest.mark.parametrize(
+    "app, expected_interface", [(asgi_app, "3.0",), (asgi2_app, "2.0",)]
+)
+def test_asgi_version(app, expected_interface):
+    config = Config(app=app)
+    config.load()
+    assert config.asgi_version == expected_interface

src/uvicorn/tests/protocols/test_http.py
@@ -674,3 +674,29 @@ def test_supported_upgrade_request(protocol_cls):
     protocol.data_received(UPGRADE_REQUEST)
 
     assert b"HTTP/1.1 426 " in protocol.transport.buffer
+
+
+async def asgi3app(scope, receive, send):
+    pass
+
+
+def asgi2app(scope):
+    async def asgi(receive, send):
+        pass
+
+    return asgi
+
+
+asgi_scope_data = [
+    (asgi3app, {"version": "3.0", "spec_version": "2.1"}),
+    (asgi2app, {"version": "2.0", "spec_version": "2.1"}),
+]
+
+
+@pytest.mark.parametrize("asgi2or3_app, expected_scopes", asgi_scope_data)
+@pytest.mark.parametrize("protocol_cls", HTTP_PROTOCOLS)
+def test_scopes(asgi2or3_app, expected_scopes, protocol_cls):
+    protocol = get_connected_protocol(asgi2or3_app, protocol_cls,)
+    protocol.data_received(SIMPLE_GET_REQUEST)
+    protocol.loop.run_one()
+    assert expected_scopes == protocol.scope.get("asgi")

src/uvicorn/uvicorn/protocols/http/h11_impl.py
@@ -188,6 +188,10 @@ class H11Protocol(asyncio.Protocol):
                 raw_path, _, query_string = event.target.partition(b"?")
                 self.scope = {
                     "type": "http",
+                    "asgi": {
+                        "version": self.config.asgi_version,
+                        "spec_version": "2.1",
+                    },
                     "http_version": event.http_version.decode("ascii"),
                     "server": self.server,
                     "client": self.client,

src/uvicorn/uvicorn/protocols/http/httptools_impl.py
@@ -214,6 +214,7 @@ class HttpToolsProtocol(asyncio.Protocol):
         self.headers = []
         self.scope = {
             "type": "http",
+            "asgi": {"version": self.config.asgi_version, "spec_version": "2.1"},
             "http_version": "1.1",
             "server": self.server,
             "client": self.client,

src/uvicorn/uvicorn/protocols/websockets/websockets_impl.py
@@ -100,6 +100,7 @@ class WebSocketProtocol(websockets.WebSocketServerProtocol):
 
         self.scope = {
             "type": "websocket",
+            "asgi": {"version": self.config.asgi_version, "spec_version": "2.1"},
             "scheme": self.scheme,
             "server": self.server,
             "client": self.client,

src/uvicorn/uvicorn/protocols/websockets/wsproto_impl.py
@@ -128,6 +128,7 @@ class WSProtocol(asyncio.Protocol):
         raw_path, _, query_string = event.target.partition("?")
         self.scope = {
             "type": "websocket",
+            "asgi": {"version": self.config.asgi_version, "spec_version": "2.1"},
             "http_version": "1.1",
             "scheme": self.scheme,
             "server": self.server,

commit 5cefbef32975807468b4194507faf3a0b0c9e3c5

Fix crash when --interface is wsgi (#730)
* Fix crash when --interface is wsgi

This is a regression introduced in
c184d5d2ae7676fcaef3623e15e57b5ebc52112c (#597)

* Set asgi 3.0

src/uvicorn/uvicorn/config.py
@@ -202,7 +202,7 @@ class Config:
 
     @property
     def asgi_version(self) -> str:
-        return {"asgi2": "2.0", "asgi3": "3.0"}[self.interface]
+        return {"asgi2": "2.0", "asgi3": "3.0", "wsgi": "3.0"}[self.interface]
 
     @property
     def is_ssl(self) -> bool:

src/uvicorn/tests/test_config.py
@@ -31,6 +31,7 @@ def test_wsgi_app():
 
     assert isinstance(config.loaded_app, WSGIMiddleware)
     assert config.interface == "wsgi"
+    assert config.asgi_version == "3.0"
 
 
 def test_proxy_headers():